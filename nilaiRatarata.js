47d44c767cf2690b1e145473cf09e74d16ee465d
yabeselkana <yabeszega1997@gmail.com>
2023-06-04 20:41:05 +0700

    perubahan ternary

--- nilaiRatarata.js
+++ nilaiRatarata.js
@@ -5,6 +5,6 @@ const ipa = 69;
 
 const nilai = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
 
-const cekNum = typeof nilai !== "number" ? " input Tidak number " : nilai > 90 ? "Nilai A" : nilai > 80 ? "Nilai B" : nilai > 70 ? " Nilai C" : nilai > 60 ? "Nilai D" : nilai > 0 ? "Nilai E" : "";
+const cekNum = typeof nilai !== "number" ? " input Tidak number " : nilai > 90 ? "Nilai A" : nilai > 80 ? "Nilai B" : nilai > 70 ? " Nilai C" : nilai > 60 ? "Nilai D" : "Nilai E";
 console.log(`Nilai = ${nilai}
 GREAD = ${cekNum}`);
