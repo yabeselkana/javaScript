a2331813c0e504efbfd05846053c6476ce3f03ef
yabeselkana <yabeszega1997@gmail.com>
2023-06-05 11:28:24 +0700

    Penambahan validasi pada nilaiRata

--- nilaiRatarata.js
+++ nilaiRatarata.js
@@ -1,14 +1,22 @@
-const mtk = 80;
+const mtk = 90;
 const bahasaIndonesia = 90;
-const bahasaInggris = 89;
-const ipa = 69;
+const bahasaInggris = 60;
+const ipa = 90;
 
-const nilai = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4;
-
-const cek = (data) => {
-  const cekNum = typeof data !== "number" ? " input Tidak number " : data > 90 ? "A" : data > 80 ? "B" : data > 70 ? "C" : data > 60 ? "D" : "Nilai E";
-  console.log(`Nilai = ${data}
+const cek = (nilai1, nilai2, nilai3, nilai4) => {
+  const validasi =
+    typeof nilai1 !== "number"
+      ? "nilai Mtk tidak ada "
+      : typeof nilai2 !== "number"
+      ? "nilai bahasa indonesia tidak ada"
+      : typeof nilai3 !== "number"
+      ? "nilai bahasa ingris tidak ada"
+      : typeof nilai4 !== "number"
+      ? "nilai Ipa tidak ada "
+      : (nilai1 + nilai2 + nilai3 + nilai4) / 4;
+  const cekNum = typeof validasi !== "number" ? "-" : validasi > 90 ? "A" : validasi > 80 ? "B" : validasi > 70 ? "C" : validasi > 60 ? "D" : "Nilai E";
+  console.log(`Nilai = ${validasi}
 GREAD = ${cekNum}`);
 };
 
-cek(nilai);
+cek(mtk, bahasaIndonesia, bahasaInggris, ipa);
